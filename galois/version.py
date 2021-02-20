8133f5f61c157bab01bde4f126c5c812db832e18
mhostetter <matthostetter@gmail.com>
2021-02-19 22:34:58 -0500

    Add version file to package manifest

--- galois/version.py
+++ galois/version.py
@@ -4,7 +4,7 @@ import os
 __version__ = None
 
 # Read version from version.txt file
-FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "version.txt")
+FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "version.txt")
 if os.path.exists(FILE):
     with open(FILE) as f:
         __version__ = f.read().split("\n")[0]
