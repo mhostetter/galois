f06f61a03b76f590ee69360bed3fb73861713f4d
mhostetter <matthostetter@gmail.com>
2021-03-03 11:02:46 -0500

    Add initial implementation of polynomial class

--- galois/__init__.py
+++ galois/__init__.py
@@ -4,3 +4,7 @@ A Python 3 package for Galois field arithmetic.
 from .version import __version__
 
 from .gf2 import GF2
+from .poly import Poly
+
+# Define the GF2 primitve polynomial here, not in gf2.py, to avoid a circular dependency
+GF2.prim_poly = Poly(GF2([1,1]))
---
+++ galois/poly.py
@@ -0,0 +1,226 @@
+import numpy as np
+
+from .gf import _GF
+from .gf2 import GF2
+
+
+class Poly:
+    """
+    asdf
+
+    Examples
+    --------
+
+    Create polynomials over GF2
+
+    .. ipython:: python
+
+        # Construct polynomials over GF2
+        a = galois.Poly([1,0,1,1])
+        b = galois.Poly([1,0,1])
+        print(a)
+        print(b)
+
+    Polynomial arithmetic in GF2
+
+    .. ipython:: python
+
+        a + b
+        # Compute the quotient of the polynomial division
+        a / b
+        # True division and floor division are equivalent
+        a / b == a // b
+        # Compute the remainder of the polynomial division
+        a % b
+    """
+
+    def __init__(self, coeffs, field=GF2):
+        assert issubclass(field, _GF)
+        if isinstance(coeffs, field):
+            self.coeffs = coeffs
+        else:
+            # Convert list or np.ndarray of integers into the specified `field`. Apply negation
+            # operator to any negative integers. For instance, `coeffs=[1, -1]` represents
+            # `x - 1` in GF2. However, the `-1` element does not exist in GF2, but the operation
+            # `-1` (the additive inverse of the `1` element) does exist.
+            c = np.array(coeffs)
+            c = np.atleast_1d(c)
+            assert c.ndim == 1, "Polynomials must only have one dimension"
+            assert np.all(np.abs(c) < field.order)
+            neg_idxs = np.where(c < 0)
+            c = np.abs(c)
+            self.coeffs = field(c)
+            c[neg_idxs] *= -1
+
+    @classmethod
+    def Powers(cls, coeffs, powers, field=GF2):
+        """
+        Examples
+        --------
+
+        .. ipython:: python
+
+            # Construct a polynomial over GF2 only specifying the non-zero terms
+            a = galois.Poly.Powers([1,1,1], [3,1,0]); a
+        """
+        powers = np.array(powers)
+        assert np.issubdtype(powers.dtype, np.integer) and np.all(powers >= 0)
+        degree = np.max(powers)
+        all_coeffs = np.zeros(degree + 1, dtype=int)
+        all_coeffs[degree - powers] = coeffs
+        return cls(all_coeffs, field=field)
+
+    def __repr__(self):
+        return "Poly({} , {})".format(self.str, type(self.coeffs).__name__)
+
+    def __str__(self):
+        return self.__repr__()
+
+    @staticmethod
+    def _verify_inputs(input1, input2):
+        a = input1.coeffs
+
+        # Verify type of second polynomial arithmetic argument and convert to a GF field type
+        if isinstance(input2, Poly):
+            b = input2.coeffs
+        elif isinstance(input2, input1.field):
+            b = input2
+        elif isinstance(input2, int):
+            assert 0 <= input2 < input1.field.order
+            b = input1.field(input2)
+        else:
+            AssertionError("Can only perform polynomial arithmetic with Poly, GF, or int classes")
+        assert type(a) is type(b), "Can only perform polynomial arthimetic between two polynomials with coefficients in the same field"
+
+        return a, b
+
+    # TODO: Speed this up with numba
+    @staticmethod
+    def divmod(dividend, divisor):
+        # q(x)*b(x) + r(x) = a(x)
+        a, b = Poly._verify_inputs(dividend, divisor)
+        field = dividend.field
+        a_degree = dividend.degree
+        b_degree = divisor.degree
+
+        if np.array_equal(a, [0]):
+            quotient = Poly([0], field=field)
+            remainder = Poly([0], field=field)
+        elif a_degree < b_degree:
+            quotient = Poly([0], field=field)
+            remainder = Poly(a, field=field)
+        else:
+            deg_q = a_degree - b_degree
+            deg_r = b_degree - 1
+            aa = field(np.append(a, field.Zeros(deg_r + 1)))
+            for i in range(0, deg_q + 1):
+                if aa[i] != 0:
+                    val = aa[i] / b[0]
+                    aa[i:i+b.size] -= val*b
+                else:
+                    val = 0
+                aa[i] = val
+            quotient = Poly(aa[0:deg_q + 1], field=field)
+            remainder = Poly(aa[deg_q + 1:deg_q + 1 + deg_r + 1], field=field)
+
+        return quotient, remainder
+
+    def __call__(self, x0):
+        assert isinstance(x0, int) or (isinstance(x0, _GF) and x0.ndim == 0)
+        x0 = self.field(x0)
+        value = self.coeffs[0]
+        for i in range(1, self.coeffs.size):
+            value = self.coeffs[i] + value*x0
+        return value
+
+    def __add__(self, other):
+        # c(x) = a(x) + b(x)
+        a, b = Poly._verify_inputs(self, other)
+        c = self.field.Zeros(max(a.size, b.size))
+        c[-a.size:] = a
+        c[-b.size:] += b
+        return Poly(c, field=self.field)
+
+    def __sub__(self, other):
+        # c(x) = a(x) - b(x)
+        a, b = Poly._verify_inputs(self, other)
+        c = self.field.Zeros(max(a.size, b.size))
+        c[-a.size:] = a
+        c[-b.size:] -= b
+        return Poly(c, field=self.field)
+
+    def __mul__(self, other):
+        # c(x) = a(x) * b(x)
+        a, b = Poly._verify_inputs(self, other)
+        a_degree = a.size - 1
+        b_degree = b.size - 1
+        c = self.field.Zeros(a_degree + b_degree + 1)
+        for i in np.nonzero(b)[0]:
+            c[i:i + a.size] += a*b[i]
+        return Poly(c, field=self.field)
+
+    def __neg__(self):
+        return Poly(-self.coeffs, field=self.field)
+
+    def __truediv__(self, other):
+        return Poly.divmod(self, other)[0]
+
+    def __floordiv__(self, other):
+        return Poly.divmod(self, other)[0]
+
+    def __mod__(self, other):
+        return Poly.divmod(self, other)[1]
+
+    def __pow__(self, other):
+        assert isinstance(other, (int, np.integer)) and other >= 0
+        # c(x) = a(x) ** b
+        a = self
+        b = other  # An integer
+        if b == 0:
+            return Poly([1], field=self.field)
+        else:
+            c = Poly(a.coeffs, field=self.field)
+            for _ in range(b-1):
+                c *= a
+            return c
+
+    def __eq__(self, other):
+        return isinstance(other, Poly) and (self.field is other.field) and (self.coeffs.shape == other.coeffs.shape) and np.all(self.coeffs == other.coeffs)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    @property
+    def coeffs(self):
+        return self._coeffs
+
+    @coeffs.setter
+    def coeffs(self, coeffs):
+        assert isinstance(coeffs, _GF), "Galois field polynomials must have coefficients belonging to a valid Galois field class (i.e. subclasses of _GF)"
+        assert coeffs.ndim == 1, "Polynomial coefficients must be arrays of 1 dimension"
+        idxs = np.nonzero(coeffs)[0]  # Non-zero indices
+        if idxs.size > 0:
+            # Trim leading non-zero powers
+            coeffs = coeffs[idxs[0]:]
+        else:
+            # All coefficients are zero, only return the x^0 place
+            field = coeffs.__class__
+            coeffs = field([0])
+        self._coeffs = coeffs
+
+    @property
+    def degree(self):
+        return self.coeffs.size - 1
+
+    @property
+    def field(self):
+        return self.coeffs.__class__
+
+    @property
+    def str(self):
+        coeffs = self.coeffs
+        degree = coeffs.size - 1
+        idxs = np.nonzero(coeffs)[0]  # Indices with non-zeros coefficients
+        x = ["{}x^{}".format(coeffs[i] if coeffs[i] != 1 else "", degree - i) for i in idxs]
+        poly_str = " + ".join(x) if x else "0"
+        return poly_str
