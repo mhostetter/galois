d27809ab345c54c19424f2701c735e24d3b343d5
mhostetter <matthostetter@gmail.com>
2021-04-10 12:37:26 -0400

    Fix bug in poly_gcd() of 1

--- tests/polys/test_function.py
+++ tests/polys/test_function.py
@@ -7,6 +7,22 @@ import numpy as np
 import galois
 
 
+def test_poly_gcd():
+    GF = galois.GF(7)
+    a = galois.Poly.Roots([2,2,2,3,5], field=GF)
+    b = galois.Poly.Roots([1,2,6], field=GF)
+    gcd, x, y = galois.poly_gcd(a, b)
+    assert a*x + b*y == gcd
+
+
+def test_poly_gcd_unit():
+    GF = galois.GF(7)
+    a = galois.conway_poly(7, 10)
+    b = galois.conway_poly(7, 5)
+    gcd = galois.poly_gcd(a, b)[0]
+    assert gcd == galois.Poly([1], field=GF)  # Note, not 3
+
+
 def test_poly_exp_mod():
     GF = galois.GF(31)
     f = galois.Poly.Random(10, field=GF)
