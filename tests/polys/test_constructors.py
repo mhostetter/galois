a4b4601828d6196ac31e20106792846c5aba0fa2
mhostetter <matthostetter@gmail.com>
2021-04-11 11:45:05 -0400

    Add multiplicities kwarg to Roots constructor

--- tests/polys/test_constructors.py
+++ tests/polys/test_constructors.py
@@ -137,3 +137,27 @@ def test_roots(poly, field):
     assert np.array_equal(p.degrees, degrees)
     assert np.array_equal(p.coeffs, coeffs)
     assert p.integer == integer
+
+
+@pytest.mark.parametrize("poly", POLY_CLASSES)
+@pytest.mark.parametrize("field", FIELDS)
+def test_roots_with_multiplicity(poly, field):
+    a = field.Random()
+    roots = [a]  # p(x) = (x - a)*(x - a)
+    multiplicities = [2]
+    degree = 2
+    degrees = [2, 1, 0]
+    coeffs = [1, -a + -a, (-a)*(-a)]
+    nonzero_degrees = [d for d, c in zip(degrees, coeffs) if c > 0]
+    nonzero_coeffs = [c for d, c in zip(degrees, coeffs) if c > 0]
+    integer = sum([int(c)*field.order**d for d, c in zip(degrees, coeffs)])
+
+    p = poly.Roots(roots, multiplicities=multiplicities, field=field)
+    assert isinstance(p, galois.Poly)
+    assert p.field is field
+    assert p.degree == degree
+    assert np.array_equal(p.nonzero_degrees, nonzero_degrees)
+    assert np.array_equal(p.nonzero_coeffs, nonzero_coeffs)
+    assert np.array_equal(p.degrees, degrees)
+    assert np.array_equal(p.coeffs, coeffs)
+    assert p.integer == integer
