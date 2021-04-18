d29f58ce27445ed42558ad4f983e7b1cb78e16eb
mhostetter <matthostetter@gmail.com>
2021-04-18 10:06:08 -0400

    Add identity matrix constructor

--- tests/fields/test_constructors.py
+++ tests/fields/test_constructors.py
@@ -21,7 +21,7 @@ def test_zeros(field, shape):
 
 
 @pytest.mark.parametrize("shape", [(), (4,), (4,4)])
-def test_zeros_valid_dtypes(field, shape):
+def test_zeros_valid_dtype(field, shape):
     dtype = valid_dtype(field)
     a = field.Zeros(shape, dtype=dtype)
     assert np.all(a == 0)
@@ -31,7 +31,7 @@ def test_zeros_valid_dtypes(field, shape):
 
 
 @pytest.mark.parametrize("shape", [(), (4,), (4,4)])
-def test_zeros_valid_dtypes(field, shape):
+def test_zeros_invalid_dtype(field, shape):
     dtype = invalid_dtype(field)
     with pytest.raises(TypeError):
         a = field.Zeros(shape, dtype=dtype)
@@ -47,7 +47,7 @@ def test_ones(field, shape):
 
 
 @pytest.mark.parametrize("shape", [(), (4,), (4,4)])
-def test_ones_valid_dtypes(field, shape):
+def test_ones_valid_dtype(field, shape):
     dtype = valid_dtype(field)
     a = field.Ones(shape, dtype=dtype)
     assert np.all(a == 1)
@@ -57,10 +57,40 @@ def test_ones_valid_dtypes(field, shape):
 
 
 @pytest.mark.parametrize("shape", [(), (4,), (4,4)])
-def test_ones_valid_dtypes(field, shape):
+def test_ones_invalid_dtype(field, shape):
     dtype = invalid_dtype(field)
     with pytest.raises(TypeError):
         a = field.Ones(shape, dtype=dtype)
+
+
+def test_eye(field):
+    size = 4
+    a = field.Eye(size)
+    for i in range(size):
+        for j in range(size):
+            assert a[i,j] == 1 if i == j else a[i,j] == 0
+    assert type(a) is field
+    assert a.dtype == field.dtypes[0]
+    assert a.shape == (size,size)
+
+
+def test_eye_valid_dtype(field):
+    dtype = valid_dtype(field)
+    size = 4
+    a = field.Eye(size, dtype=dtype)
+    for i in range(size):
+        for j in range(size):
+            assert a[i,j] == 1 if i == j else a[i,j] == 0
+    assert type(a) is field
+    assert a.dtype == dtype
+    assert a.shape == (size,size)
+
+
+def test_eye_invalid_dtype(field):
+    dtype = invalid_dtype(field)
+    size = 4
+    with pytest.raises(TypeError):
+        a = field.Eye(size, dtype=dtype)
 
 
 @pytest.mark.parametrize("shape", [(), (4,), (4,4)])
@@ -73,7 +103,7 @@ def test_random(field, shape):
 
 
 @pytest.mark.parametrize("shape", [(), (4,), (4,4)])
-def test_random_valid_dtypes(field, shape):
+def test_random_valid_dtype(field, shape):
     dtype = valid_dtype(field)
     a = field.Random(shape, dtype=dtype)
     assert np.all(a >= 0) and np.all(a < field.order)
@@ -83,7 +113,7 @@ def test_random_valid_dtypes(field, shape):
 
 
 @pytest.mark.parametrize("shape", [(), (4,), (4,4)])
-def test_random_valid_dtypes(field, shape):
+def test_random_invalid_dtype(field, shape):
     dtype = invalid_dtype(field)
     with pytest.raises(TypeError):
         a = field.Random(shape, dtype=dtype)
