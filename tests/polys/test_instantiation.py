40ec58e8a87212364996bbc34f744e978b1cfb75
mhostetter <matthostetter@gmail.com>
2021-04-25 14:40:21 -0400

    Add BinaryPoly implementation for speed-ups of poly arithmetic in GF(2)

--- tests/polys/test_instantiation.py
+++ tests/polys/test_instantiation.py
@@ -47,182 +47,67 @@ def config(field):
     return d
 
 
-class TestPoly:
-    @pytest.mark.parametrize("type1", [list, tuple, np.array, galois.GFArray])
-    def test_coeffs(self, type1, config):
-        GF = config["GF"]
-        if type1 is not galois.GFArray:
-            p = galois.Poly(type1(config["coeffs"]), field=GF)
-        else:
-            p = galois.Poly(GF(config["coeffs"]))
-        self.check_attributes(p, config)
-
-    @pytest.mark.parametrize("type1", [list, tuple, np.array, galois.GFArray])
-    def test_leading_zero_coeffs(self, type1, config):
-        GF = config["GF"]
-        if type1 is not galois.GFArray:
-            p = galois.Poly(type1(config["lz_coeffs"]), field=GF)
-        else:
-            p = galois.Poly(GF(config["lz_coeffs"]))
-        self.check_attributes(p, config)
-
-    @pytest.mark.parametrize("type1", [list, tuple, np.array, galois.GFArray])
-    def test_ascending_coeffs(self, type1, config):
-        GF = config["GF"]
-        if type1 is not galois.GFArray:
-            p = galois.Poly(type1(config["coeffs"][::-1]), field=GF, order="asc")
-        else:
-            p = galois.Poly(GF(config["coeffs"][::-1]), order="asc")
-        self.check_attributes(p, config)
-
-    @pytest.mark.parametrize("type1", [list, tuple, np.array])
-    def test_negative_coeffs(self, type1, config):
-        GF = config["GF"]
-        p = galois.Poly(type1(config["neg_coeffs"]), field=GF)
-        self.check_attributes(p, config)
-
-    @pytest.mark.parametrize("type1", [list, tuple, np.array, galois.GFArray])
-    @pytest.mark.parametrize("field", FIELDS)
-    def test_zero(self, type1, field):
-        # The zero polynomial can cause problems, so test it explicitly
-        if type1 is not galois.GFArray:
-            p = galois.Poly(type1([0]), field=field)
-        else:
-            p = galois.Poly(field([0]))
-        assert isinstance(p, galois.Poly)
-        assert p.field is field
-        assert p.degree == 0
-        assert np.array_equal(p.nonzero_degrees, [])
-        assert np.array_equal(p.nonzero_coeffs, [])
-        assert np.array_equal(p.degrees, [0])
-        assert np.array_equal(p.coeffs, [0])
-        assert p.integer == 0
-
-    def check_attributes(self, poly, config):
-        assert isinstance(poly, galois.Poly)
-        assert poly.field is config["GF"]
-        assert poly.degree == config["degree"]
-        assert np.array_equal(poly.nonzero_degrees, config["nonzero_degrees"])
-        assert np.array_equal(poly.nonzero_coeffs, config["nonzero_coeffs"])
-        assert np.array_equal(poly.degrees, config["degrees"])
-        assert np.array_equal(poly.coeffs, config["coeffs"])
-        assert poly.integer == config["integer"]
+@pytest.mark.parametrize("type1", [list, tuple, np.array, galois.GFArray])
+def test_coeffs(type1, config):
+    GF = config["GF"]
+    if type1 is not galois.GFArray:
+        p = galois.Poly(type1(config["coeffs"]), field=GF)
+    else:
+        p = galois.Poly(GF(config["coeffs"]))
+    check_attributes(p, config)
 
 
-class TestDensePoly:
-    @pytest.mark.parametrize("type1", [list, tuple, np.array, galois.GFArray])
-    def test_coeffs(self, type1, config):
-        GF = config["GF"]
-        if type1 is not galois.GFArray:
-            p = galois.poly.DensePoly(type1(config["coeffs"]), field=GF)
-        else:
-            p = galois.poly.DensePoly(GF(config["coeffs"]))
-        self.check_attributes(p, config)
-
-    @pytest.mark.parametrize("type1", [list, tuple, np.array, galois.GFArray])
-    def test_leading_zero_coeffs(self, type1, config):
-        GF = config["GF"]
-        if type1 is not galois.GFArray:
-            p = galois.poly.DensePoly(type1(config["lz_coeffs"]), field=GF)
-        else:
-            p = galois.poly.DensePoly(GF(config["lz_coeffs"]))
-        self.check_attributes(p, config)
-
-    @pytest.mark.parametrize("type1", [list, tuple, np.array, galois.GFArray])
-    def test_ascending_coeffs(self, type1, config):
-        GF = config["GF"]
-        if type1 is not galois.GFArray:
-            p = galois.poly.DensePoly(type1(config["coeffs"][::-1]), field=GF, order="asc")
-        else:
-            p = galois.poly.DensePoly(GF(config["coeffs"][::-1]), order="asc")
-        self.check_attributes(p, config)
-
-    @pytest.mark.parametrize("type1", [list, tuple, np.array])
-    def test_negative_coeffs(self, type1, config):
-        GF = config["GF"]
-        p = galois.poly.DensePoly(type1(config["neg_coeffs"]), field=GF)
-        self.check_attributes(p, config)
-
-    @pytest.mark.parametrize("type1", [list, tuple, np.array, galois.GFArray])
-    @pytest.mark.parametrize("field", FIELDS)
-    def test_zero(self, type1, field):
-        # The zero polynomial can cause problems, so test it explicitly
-        if type1 is not galois.GFArray:
-            p = galois.poly.DensePoly(type1([0]), field=field)
-        else:
-            p = galois.poly.DensePoly(field([0]))
-        assert type(p) is galois.poly.DensePoly
-        assert p.field is field
-        assert p.degree == 0
-        assert np.array_equal(p.nonzero_degrees, [])
-        assert np.array_equal(p.nonzero_coeffs, [])
-        assert np.array_equal(p.degrees, [0])
-        assert np.array_equal(p.coeffs, [0])
-        assert p.integer == 0
-
-    def check_attributes(self, poly, config):
-        assert type(poly) is galois.poly.DensePoly
-        assert poly.field is config["GF"]
-        assert poly.degree == config["degree"]
-        assert np.array_equal(poly.nonzero_degrees, config["nonzero_degrees"])
-        assert np.array_equal(poly.nonzero_coeffs, config["nonzero_coeffs"])
-        assert np.array_equal(poly.degrees, config["degrees"])
-        assert np.array_equal(poly.coeffs, config["coeffs"])
-        assert poly.integer == config["integer"]
+@pytest.mark.parametrize("type1", [list, tuple, np.array, galois.GFArray])
+def test_leading_zero_coeffs(type1, config):
+    GF = config["GF"]
+    if type1 is not galois.GFArray:
+        p = galois.Poly(type1(config["lz_coeffs"]), field=GF)
+    else:
+        p = galois.Poly(GF(config["lz_coeffs"]))
+    check_attributes(p, config)
 
 
-class TestSparsePoly:
-    @pytest.mark.parametrize("type1", [list, tuple, np.array])
-    @pytest.mark.parametrize("type2", [list, tuple, np.array, galois.GFArray])
-    def test_coeffs(self, type1, type2, config):
-        GF = config["GF"]
-        if type2 is not galois.GFArray:
-            p = galois.poly.SparsePoly(type1(config["nonzero_degrees"]), type2(config["nonzero_coeffs"]), field=GF)
-        else:
-            p = galois.poly.SparsePoly(type1(config["nonzero_degrees"]), GF(config["nonzero_coeffs"]))
-        self.check_attributes(p, config)
+@pytest.mark.parametrize("type1", [list, tuple, np.array, galois.GFArray])
+def test_ascending_coeffs(type1, config):
+    GF = config["GF"]
+    if type1 is not galois.GFArray:
+        p = galois.Poly(type1(config["coeffs"][::-1]), field=GF, order="asc")
+    else:
+        p = galois.Poly(GF(config["coeffs"][::-1]), order="asc")
+    check_attributes(p, config)
 
-    @pytest.mark.parametrize("type1", [list, tuple, np.array])
-    @pytest.mark.parametrize("type2", [list, tuple, np.array, galois.GFArray])
-    def test_zeros_coeffs(self, type1, type2, config):
-        GF = config["GF"]
-        if type2 is not galois.GFArray:
-            p = galois.poly.SparsePoly(type1(config["mz_nonzero_degrees"]), type2(config["mz_nonzero_coeffs"]), field=GF)
-        else:
-            p = galois.poly.SparsePoly(type1(config["mz_nonzero_degrees"]), GF(config["mz_nonzero_coeffs"]))
-        self.check_attributes(p, config)
 
-    @pytest.mark.parametrize("type1", [list, tuple, np.array])
-    @pytest.mark.parametrize("type2", [list, tuple, np.array])
-    def test_negative_coeffs(self, type1, type2, config):
-        GF = config["GF"]
-        p = galois.poly.SparsePoly(type1(config["nonzero_degrees"]), type2(config["neg_nonzero_coeffs"]), field=GF)
-        self.check_attributes(p, config)
+@pytest.mark.parametrize("type1", [list, tuple, np.array])
+def test_negative_coeffs(type1, config):
+    GF = config["GF"]
+    p = galois.Poly(type1(config["neg_coeffs"]), field=GF)
+    check_attributes(p, config)
 
-    @pytest.mark.parametrize("type1", [list, tuple, np.array])
-    @pytest.mark.parametrize("type2", [list, tuple, np.array, galois.GFArray])
-    @pytest.mark.parametrize("field", FIELDS)
-    def test_zero(self, type1, type2, field):
-        # The zero polynomial can cause problems, so test it explicitly
-        if type2 is not galois.GFArray:
-            p = galois.poly.SparsePoly(type1([0]), type2([0]), field=field)
-        else:
-            p = galois.poly.SparsePoly(type1([0]), field([0]))
-        assert type(p) is galois.poly.SparsePoly
-        assert p.field is field
-        assert p.degree == 0
-        assert np.array_equal(p.nonzero_degrees, [])
-        assert np.array_equal(p.nonzero_coeffs, [])
-        assert np.array_equal(p.degrees, [0])
-        assert np.array_equal(p.coeffs, [0])
-        assert p.integer == 0
 
-    def check_attributes(self, poly, config):
-        assert type(poly) is galois.poly.SparsePoly
-        assert poly.field is config["GF"]
-        assert poly.degree == config["degree"]
-        assert np.array_equal(poly.nonzero_degrees, config["nonzero_degrees"])
-        assert np.array_equal(poly.nonzero_coeffs, config["nonzero_coeffs"])
-        assert np.array_equal(poly.degrees, config["degrees"])
-        assert np.array_equal(poly.coeffs, config["coeffs"])
-        assert poly.integer == config["integer"]
+@pytest.mark.parametrize("type1", [list, tuple, np.array, galois.GFArray])
+@pytest.mark.parametrize("field", FIELDS)
+def test_zero(type1, field):
+    # The zero polynomial can cause problems, so test it explicitly
+    if type1 is not galois.GFArray:
+        p = galois.Poly(type1([0]), field=field)
+    else:
+        p = galois.Poly(field([0]))
+    assert isinstance(p, galois.Poly)
+    assert p.field is field
+    assert p.degree == 0
+    assert np.array_equal(p.nonzero_degrees, [])
+    assert np.array_equal(p.nonzero_coeffs, [])
+    assert np.array_equal(p.degrees, [0])
+    assert np.array_equal(p.coeffs, [0])
+    assert p.integer == 0
+
+
+def check_attributes(poly, config):
+    assert isinstance(poly, galois.Poly)
+    assert poly.field is config["GF"]
+    assert poly.degree == config["degree"]
+    assert np.array_equal(poly.nonzero_degrees, config["nonzero_degrees"])
+    assert np.array_equal(poly.nonzero_coeffs, config["nonzero_coeffs"])
+    assert np.array_equal(poly.degrees, config["degrees"])
+    assert np.array_equal(poly.coeffs, config["coeffs"])
+    assert poly.integer == config["integer"]
