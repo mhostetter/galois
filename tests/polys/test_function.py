e7a8688a63e0efade0901e775ebde61d4007a400
BK-Modding <kavesbteja@gmail.com>
2021-05-07 20:31:22 +0530

    Unit test added for modified poly_gcd() and fixed linting errors

--- tests/polys/test_function.py
+++ tests/polys/test_function.py
@@ -14,6 +14,12 @@ def test_poly_gcd():
     gcd, x, y = galois.poly_gcd(a, b)
     assert a*x + b*y == gcd
 
+def test_poly_gcd_2():
+    GF = galois.GF(2**5, irreducible_poly=galois.Poly(list(reversed([1, 0, 1, 0, 0, 1]))))
+    a = galois.Poly([1, 2, 4, 1], field=GF)
+    b = galois.Poly([9, 3], field=GF)
+    gcd, x, y = galois.poly_gcd(a, b)
+    assert a*x + b*y == gcd
 
 def test_poly_gcd_unit():
     GF = galois.GF(7)
