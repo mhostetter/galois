842022f3c128a74ca012f60a0fa5dd1ec62eec3e
mhostetter <matthostetter@gmail.com>
2021-05-12 14:33:24 -0400

    Update naive log to be more general

--- galois/log.py
+++ galois/log.py
@@ -3,7 +3,7 @@ A module containing algorithms for computing discrete logarithms.
 """
 import math
 
-from .modular import euler_totient, is_cyclic, is_primitive_root
+from .modular import order
 from .overrides import set_module
 
 __all__ = ["log_naive"]
@@ -15,15 +15,20 @@ def log_naive(beta, alpha, modulus):
     Computes the discrete logarithm :math:`x = \\textrm{log}_{\\alpha}(\\beta)\\ (\\textrm{mod}\\ m)`.
 
     This function implements the naive algorithm. It is included for testing and reference.
+    This method take :math:`O(n)` multiplications, where :math:`n` is the multiplicative order of :math:`\\alpha`.
 
     Arguments
     ---------
     beta : int
-        The integer :math:`\\beta` to compute the logarithm of.
+        The integer :math:`\\beta` to compute the logarithm of. :math:`\\beta` must be coprime to :math:`m`.
     alpha : int
-        The base :math:`\\alpha`.
+        The base :math:`\\alpha`. :math:`\\alpha` must be coprime to :math:`m`.
     modulus : int
         The modulus :math:`m`.
+
+    References
+    ----------
+    * Chapter 3.6.1 from https://cacr.uwaterloo.ca/hac/about/chap3.pdf
 
     Examples
     --------
@@ -38,21 +43,24 @@ def log_naive(beta, alpha, modulus):
     .. ipython:: python
 
         N = 18
+
+        # alpha and beta must be totatives of the modulus, i.e. coprime to N
         galois.totatives(N)
+
+        # The discrete log is defined for all totatives base a primitive root
         galois.primitive_roots(N)
+
         x = galois.log_naive(11, 5, N); x
-        5**x % N
+        pow(5, x, N)
     """
-    if not is_cyclic(modulus):
-        raise ValueError(f"Argument `modulus` must produce a multiplicative group that is cyclic, Z({modulus})* is not cyclic.")
     if not math.gcd(beta, modulus) == 1:
         raise ValueError(f"Argument `beta` must be coprime with `modulus`, {beta} is not coprime with {modulus}.")
-    if not is_primitive_root(alpha, modulus):
-        raise ValueError(f"Argument `alpha` must be a primitive root of `modulus`, {alpha} is not a primitive root of {modulus}.")
+    if not math.gcd(alpha, modulus) == 1:
+        raise ValueError(f"Argument `alpha` must be coprime with `modulus`, {alpha} is not coprime with {modulus}.")
 
-    order = euler_totient(modulus)
-    for k in range(order):
+    order_ = order(alpha, modulus)
+    for k in range(order_):
         if pow(alpha, k, modulus) == beta:
             return k
 
-    raise RuntimeError(f"Discrete logarithm of {beta} base {alpha} mod {modulus} was not found.")
+    raise ValueError(f"The discrete logarithm of {beta} base {alpha} mod {modulus} does not exist.")
