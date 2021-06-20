4821550e3e8cd3ce9d998637704e3a969c5d0a88
mhostetter <matthostetter@gmail.com>
2021-06-20 09:36:51 -0400

    Add error detection to RS codes

--- tests/codes/helper.py
+++ tests/codes/helper.py
@@ -4,6 +4,7 @@ import numpy as np
 
 
 def random_errors(GF, N, n, max_errors):
+    max_errors = min(n, max_errors)
     N_errors = np.random.randint(0, max_errors + 1, N)
     N_errors[0] = max_errors  # Ensure the max number of errors is present at least once
 
