c3efa00059ae35a9127f2f5fb74983e24e32352f
mhostetter <matthostetter@gmail.com>
2021-08-01 15:34:20 -0400

    In properties string, represent the primitive element as a polynomial

--- tests/fields/test_classes.py
+++ tests/fields/test_classes.py
@@ -57,4 +57,4 @@ def test_cant_set_attribute(attribute):
 
 def test_properties():
     GF = galois.GF(2**8)
-    assert GF.properties == "GF(2^8):\n  characteristic: 2\n  degree: 8\n  order: 256\n  irreducible_poly: Poly(x^8 + x^4 + x^3 + x^2 + 1, GF(2))\n  is_primitive_poly: True\n  primitive_element: GF(2, order=2^8)"
+    assert GF.properties == "GF(2^8):\n  characteristic: 2\n  degree: 8\n  order: 256\n  irreducible_poly: x^8 + x^4 + x^3 + x^2 + 1\n  is_primitive_poly: True\n  primitive_element: x"
