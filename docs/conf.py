b5e633e8649d40d4a8785164af4100a25fafdb18
mhostetter <matthostetter@gmail.com>
2021-08-07 14:20:08 -0400

    Remove `nbsphinx` docs dependency

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,6 @@ extensions = [
     'sphinx.ext.intersphinx',
     'recommonmark',
     'sphinx_rtd_theme',
-    'nbsphinx',
     'IPython.sphinxext.ipython_console_highlighting',
     'IPython.sphinxext.ipython_directive'
 ]
