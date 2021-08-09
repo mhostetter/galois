b0e79796dab75007e11964cf23aa72537e0f2b7e
mhostetter <matthostetter@gmail.com>
2021-08-09 16:31:52 -0400

    Increase LFSR code coverage

--- tests/test_fibonacci_lfsr.py
+++ tests/test_fibonacci_lfsr.py
@@ -7,6 +7,54 @@ import numpy as np
 import galois
 
 
+def test_exceptions():
+    poly = galois.Poly.Degrees([7,1,0])
+
+    with pytest.raises(TypeError):
+        galois.LFSR(poly.coeffs)
+    with pytest.raises(TypeError):
+        galois.LFSR(poly, state=float(poly.integer))
+    with pytest.raises(TypeError):
+        galois.LFSR(poly.coeffs, config=1)
+
+    with pytest.raises(ValueError):
+        galois.LFSR(poly, config="invalid-argument")
+
+
+def test_state():
+    poly = galois.Poly.Degrees([7,1,0])
+
+    lfsr = galois.LFSR(poly, state=1)
+    assert np.array_equal(lfsr.state, [0, 0, 0, 0, 0, 0, 1])
+
+    lfsr = galois.LFSR(poly, state=4)
+    assert np.array_equal(lfsr.state, [0, 0, 0, 0, 1, 0, 0])
+
+
+def test_str():
+    poly = galois.Poly.Degrees([7,1,0])
+    lfsr = galois.LFSR(poly)
+    assert str(lfsr) == "<Fibonacci LFSR: poly=Poly(x^7 + x + 1, GF(2))>"
+
+
+def test_repr():
+    poly = galois.Poly.Degrees([7,1,0])
+    lfsr = galois.LFSR(poly)
+    assert repr(lfsr) == "<Fibonacci LFSR: poly=Poly(x^7 + x + 1, GF(2))>"
+
+
+def test_step_exceptions():
+    poly = galois.Poly.Degrees([7,1,0])
+    lfsr = galois.LFSR(poly)
+
+    with pytest.raises(TypeError):
+        lfsr.step(10.0)
+    with pytest.raises(ValueError):
+        lfsr.step(0)
+    with pytest.raises(ValueError):
+        lfsr.step(-1)
+
+
 def test_output_is_reversed_state():
     poly = galois.Poly.Degrees([7,1,0])
     state = galois.GF2.Zeros(7)
@@ -18,7 +66,7 @@ def test_output_is_reversed_state():
 
 def test_gf2_output_1():
     """
-    SageMath:
+    Sage:
         F = GF(2)
         o = F(0); l = F(1)
         key = [l,o,o,l]
@@ -37,7 +85,7 @@ def test_gf2_output_1():
 
 def test_gf2_output_2():
     """
-    SageMath:
+    Sage:
         F = GF(2)
         o = F(0); l = F(1)
         key = [l,l,o,l]
@@ -56,7 +104,7 @@ def test_gf2_output_2():
 
 def test_gf2_output_3():
     """
-    SageMath:
+    Sage:
         F = GF(2)
         o = F(0); l = F(1)
         key = [l,o,o,o,l,l,l,o]
@@ -75,7 +123,7 @@ def test_gf2_output_3():
 
 def test_gf3_output():
     """
-    SageMath:
+    Sage:
         F = GF(3)
         key = [F(2),F(0),F(0),F(2)]
         fill = [F(1),F(0),F(0),F(0)]
@@ -93,7 +141,7 @@ def test_gf3_output():
 
 def test_gf5_output():
     """
-    SageMath:
+    Sage:
         F = GF(5)
         key = [F(3),F(0),F(3),F(1)]
         fill = [F(1),F(0),F(0),F(0)]
@@ -109,12 +157,47 @@ def test_gf5_output():
     assert np.array_equal(y, y_truth)
 
 
+def test_gfp_large_output():
+    """
+    Sage:
+        F = GF(36893488147419103183)
+        key = [F(3),F(0),F(3),F(1)]
+        fill = [F(1),F(0),F(0),F(0)]
+        n = 20
+        s = lfsr_sequence(key,fill,n); np.array(s)
+    """
+    GF = galois.GF(36893488147419103183)
+    poly = galois.Poly([3,0,3,1,1], field=GF)  # Not a primitive polynomial...
+    state = GF([0,0,0,1])
+    y_truth = GF([1, 0, 0, 0, 3, 3, 12, 21, 66, 138, 372, 849, 2163, 5124, 12729, 30648, 75324, 182640, 446799, 1086663, 2653032, 6460941, 15760434, 38403246, 93643644, 228236205, 556448439, 1356366792, 3306643041, 8060452032, 19649726472, 47900182944, 116769291483, 284651196411, 693908250276, 1691562388341, 4123595013618, 10052235767874, 24504745559556, 59736140028201])
+    lfsr = galois.LFSR(poly, state=state)
+    y = lfsr.step(y_truth.size)
+    assert np.array_equal(y, y_truth)
+
+
+def test_berlekamp_massey_exceptions():
+    GF = galois.GF2
+    s = GF([0,0,1,1,0,1,1,1,0,1])
+
+    with pytest.raises(TypeError):
+        galois.berlekamp_massey(s.view(np.ndarray))
+    with pytest.raises(TypeError):
+        galois.berlekamp_massey(s, config=1)
+    with pytest.raises(TypeError):
+        galois.berlekamp_massey(s, state=1)
+
+    with pytest.raises(ValueError):
+        galois.berlekamp_massey(np.atleast_2d(s))
+    with pytest.raises(ValueError):
+        galois.berlekamp_massey(s, config="invalid-argument")
+
+
 def test_berlekamp_massey_gf2_1():
     """
-    SageMath:
+    Sage:
         F = GF(2)
         s = [0,0,1,1,0,1,1,1,0,1]
-        s = [F(1) if si == 1 else F(0) for si in s]
+        s = [F(si) for si in s]
         berlekamp_massey(s).reverse()  # Sage defines polynomial backwards
     """
     GF = galois.GF2
@@ -126,10 +209,10 @@ def test_berlekamp_massey_gf2_1():
 
 def test_berlekamp_massey_gf2_2():
     """
-    SageMath:
+    Sage:
         F = GF(2)
         s = [1,0,0,0,0,0,0,0,1,0,1,1,0,0,0,1,1,1,1,0]
-        s = [F(1) if si == 1 else F(0) for si in s]
+        s = [F(si) for si in s]
         berlekamp_massey(s).reverse()  # Sage defines polynomial backwards
     """
     GF = galois.GF2
@@ -141,10 +224,10 @@ def test_berlekamp_massey_gf2_2():
 
 def test_berlekamp_massey_gf2_3():
     """
-    SageMath:
+    Sage:
         F = GF(2)
         s = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,1,0,0,1,0,1,0,1,1,1,1,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,0,1,0,1,0,1,1,0,0,1,1,0,0,0,0,0,1,1,1,0,1,1,1,1,0,0,0,1,0,1,0,1,0,0,0,0,1,1,0,1,0,1,1,1,1,1,1,0,1,0,1,0,1,0,0,0,1,0,0,1,1,1,1,0,0,0,0,1,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1,0,0,1,1,1,0,1,0,1,0,0,0,1,0,0,1,0,1,0,0,1,0,1,1,1,1,1,1,1,1,0,0,0,1,1,0,1,0,1,1,1,0,1,1,0,0,0,1,0,0,1,1,1,0,0,1,1,0,1,1,1,1,0,1,0,0,0,0,1,0,1,0,0,0,1,0,0,1,1,1,1,1,1,0,1,1]
-        s = [F(1) if si == 1 else F(0) for si in s]
+        s = [F(si) for si in s]
         berlekamp_massey(s).reverse()  # Sage defines polynomial backwards
     """
     GF = galois.GF2
@@ -152,3 +235,18 @@ def test_berlekamp_massey_gf2_3():
     c_truth = galois.Poly.Degrees([100,57,56,55,52,48,47,46,45,44,43,41,37,36,35,34,31,30,27,25,24,22,20,19,16,15,11,9,8,6,5,3,0])
     c = galois.berlekamp_massey(s)
     assert c == c_truth
+
+
+# def test_berlekamp_massey_gfp_large():
+#     """
+#     Sage:
+#         F = GF(36893488147419103183)
+#         s = [1, 0, 0, 0, 3, 3, 12, 21, 66, 138, 372, 849, 2163, 5124, 12729, 30648, 75324, 182640, 446799, 1086663, 2653032, 6460941, 15760434, 38403246, 93643644, 228236205, 556448439, 1356366792, 3306643041, 8060452032, 19649726472, 47900182944, 116769291483, 284651196411, 693908250276, 1691562388341, 4123595013618, 10052235767874, 24504745559556, 59736140028201]
+#         s = [F(si) for si in s]
+#         berlekamp_massey(s).reverse()  # Sage defines polynomial backwards
+#     """
+#     GF = galois.GF(36893488147419103183)
+#     s = GF([1, 0, 0, 0, 3, 3, 12, 21, 66, 138, 372, 849, 2163, 5124, 12729, 30648, 75324, 182640, 446799, 1086663, 2653032, 6460941, 15760434, 38403246, 93643644, 228236205, 556448439, 1356366792, 3306643041, 8060452032, 19649726472, 47900182944, 116769291483, 284651196411, 693908250276, 1691562388341, 4123595013618, 10052235767874, 24504745559556, 59736140028201])
+#     c_truth = galois.Poly([36893488147419103180, 0, 36893488147419103180, 36893488147419103182, 1], field=GF)
+#     c = galois.berlekamp_massey(s)
+#     assert c == c_truth
--- tests/test_galois_lfsr.py
+++ tests/test_galois_lfsr.py
@@ -7,6 +7,54 @@ import numpy as np
 import galois
 
 
+def test_exceptions():
+    poly = galois.Poly.Degrees([7,1,0])
+
+    with pytest.raises(TypeError):
+        galois.LFSR(poly.coeffs)
+    with pytest.raises(TypeError):
+        galois.LFSR(poly, state=float(poly.integer))
+    with pytest.raises(TypeError):
+        galois.LFSR(poly.coeffs, config=1)
+
+    with pytest.raises(ValueError):
+        galois.LFSR(poly, config="invalid-argument")
+
+
+def test_state():
+    poly = galois.Poly.Degrees([7,1,0])
+
+    lfsr = galois.LFSR(poly, state=1, config="galois")
+    assert np.array_equal(lfsr.state, [0, 0, 0, 0, 0, 0, 1])
+
+    lfsr = galois.LFSR(poly, state=4, config="galois")
+    assert np.array_equal(lfsr.state, [0, 0, 0, 0, 1, 0, 0])
+
+
+def test_str():
+    poly = galois.Poly.Degrees([7,1,0])
+    lfsr = galois.LFSR(poly, config="galois")
+    assert str(lfsr) == "<Galois LFSR: poly=Poly(x^7 + x + 1, GF(2))>"
+
+
+def test_repr():
+    poly = galois.Poly.Degrees([7,1,0])
+    lfsr = galois.LFSR(poly, config="galois")
+    assert repr(lfsr) == "<Galois LFSR: poly=Poly(x^7 + x + 1, GF(2))>"
+
+
+def test_step_exceptions():
+    poly = galois.Poly.Degrees([7,1,0])
+    lfsr = galois.LFSR(poly, config="galois")
+
+    with pytest.raises(TypeError):
+        lfsr.step(10.0)
+    with pytest.raises(ValueError):
+        lfsr.step(0)
+    with pytest.raises(ValueError):
+        lfsr.step(-1)
+
+
 def test_gf2_output_1():
     """
     The states of the Galois LFSR generate the binary extension field with the connection polynomial as its
@@ -43,9 +91,26 @@ def test_gf2_output_2():
         lfsr.step()
 
 
+def test_berlekamp_massey_exceptions():
+    GF = galois.GF2
+    s = GF([0,0,1,1,0,1,1,1,0,1])
+
+    with pytest.raises(TypeError):
+        galois.berlekamp_massey(s.view(np.ndarray))
+    with pytest.raises(TypeError):
+        galois.berlekamp_massey(s, config=1)
+    with pytest.raises(TypeError):
+        galois.berlekamp_massey(s, state=1)
+
+    with pytest.raises(ValueError):
+        galois.berlekamp_massey(np.atleast_2d(s))
+    with pytest.raises(ValueError):
+        galois.berlekamp_massey(s, config="invalid-argument")
+
+
 def test_berlekamp_massey_gf2_1():
     """
-    SageMath:
+    Sage:
         F = GF(2)
         s = [0,0,1,1,0,1,1,1,0,1]
         s = [F(1) if si == 1 else F(0) for si in s]
@@ -60,7 +125,7 @@ def test_berlekamp_massey_gf2_1():
 
 def test_berlekamp_massey_gf2_2():
     """
-    SageMath:
+    Sage:
         F = GF(2)
         s = [1,0,0,0,0,0,0,0,1,0,1,1,0,0,0,1,1,1,1,0]
         s = [F(1) if si == 1 else F(0) for si in s]
@@ -75,7 +140,7 @@ def test_berlekamp_massey_gf2_2():
 
 def test_berlekamp_massey_gf2_3():
     """
-    SageMath:
+    Sage:
         F = GF(2)
         s = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,1,0,0,1,0,1,0,1,1,1,1,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,0,1,0,1,0,1,1,0,0,1,1,0,0,0,0,0,1,1,1,0,1,1,1,1,0,0,0,1,0,1,0,1,0,0,0,0,1,1,0,1,0,1,1,1,1,1,1,0,1,0,1,0,1,0,0,0,1,0,0,1,1,1,1,0,0,0,0,1,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1,0,0,1,1,1,0,1,0,1,0,0,0,1,0,0,1,0,1,0,0,1,0,1,1,1,1,1,1,1,1,0,0,0,1,1,0,1,0,1,1,1,0,1,1,0,0,0,1,0,0,1,1,1,0,0,1,1,0,1,1,1,1,0,1,0,0,0,0,1,0,1,0,0,0,1,0,0,1,1,1,1,1,1,0,1,1]
         s = [F(1) if si == 1 else F(0) for si in s]
