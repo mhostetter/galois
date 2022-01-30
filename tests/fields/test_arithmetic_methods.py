b6abfc29b24eea268e5f16e40800d81aa8422191
mhostetter <matthostetter@gmail.com>
2022-01-30 09:47:55 -0500

    Add more `multiplicative_order()` unit tests

--- tests/fields/test_arithmetic_methods.py
+++ tests/fields/test_arithmetic_methods.py
@@ -16,3 +16,21 @@ def test_additive_order(field_additive_order):
     z = x.additive_order()
     assert np.array_equal(z, Z)
     assert type(z) is np.ndarray
+
+
+def test_multiplicative_order(field_multiplicative_order):
+    GF, X, Z = field_multiplicative_order["GF"], field_multiplicative_order["X"], field_multiplicative_order["Z"]
+    if GF.dtypes[-1] == np.object_:
+        # FIXME: Skipping large fields because they're too slow
+        return
+
+    dtype = random.choice(GF.dtypes)
+    x = X.astype(dtype)
+    z = x.multiplicative_order()
+    assert np.array_equal(z, Z)
+    assert type(z) is np.ndarray
+
+    with pytest.raises(ArithmeticError):
+        GF(0).multiplicative_order()
+    with pytest.raises(ArithmeticError):
+        GF.Range(0, 2).multiplicative_order()
