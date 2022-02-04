d87cb879299a73744c76b129fdc28eb85339db8b
mhostetter <matthostetter@gmail.com>
2022-02-04 10:31:24 -0500

    Add more unit tests for integer LCM

--- scripts/generate_int_test_vectors.py
+++ scripts/generate_int_test_vectors.py
@@ -11,7 +11,7 @@ import shutil
 
 import sage
 import numpy as np
-from sage.all import xgcd
+from sage.all import xgcd, lcm
 
 PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "tests")
 FOLDER = os.path.join(PATH, "data")
@@ -52,3 +52,13 @@ for i in range(len(X)):
     T[i] = int(t)
 d = {"X": X, "Y": Y, "D": D, "S": S, "T": T}
 save_pickle(d, FOLDER, "egcd.pkl")
+
+set_seed(SEED + 102)
+X = [[random.randint(-1000, 1000) for _ in range(random.randint(2, 6))] for _ in range(20)] + [[random.randint(-1000, 1_000_000) for _ in range(random.randint(2, 6))] for _ in range(20)]
+Z = [0,]*len(X)
+for i in range(len(X)):
+    x = X[i]
+    z = lcm(x)
+    Z[i] = int(z)
+d = {"X": X, "Z": Z}
+save_pickle(d, FOLDER, "lcm.pkl")
