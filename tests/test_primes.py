fff78a1b60f09a28fc47394eb5aa867c7e83e04f
mhostetter <matthostetter@gmail.com>
2022-12-12 10:52:50 -0500

    Add newly discovered Mersenne exponents

--- tests/test_primes.py
+++ tests/test_primes.py
@@ -100,9 +100,6 @@ def test_mersenne_exponents():
     exponents = [2, 3, 5, 7, 13, 17, 19, 31, 61, 89, 107, 127]
     assert galois.mersenne_exponents(128) == exponents
 
-    exponents = [2, 3, 5, 7, 13, 17, 19, 31, 61, 89, 107, 127, 521, 607, 1279, 2203, 2281, 3217, 4253, 4423, 9689, 9941, 11213, 19937, 21701, 23209, 44497, 86243, 110503, 132049, 216091, 756839, 859433, 1257787, 1398269, 2976221, 3021377, 6972593, 13466917, 20996011, 24036583, 25964951, 30402457, 32582657, 37156667, 42643801, 43112609]  # fmt: skip
-    assert galois.mersenne_exponents() == exponents
-
 
 def test_mersenne_primes_exceptions():
     with pytest.raises(TypeError):
