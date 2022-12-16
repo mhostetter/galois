15e4d96ce01556e5ed0bb79c0b7daadd2ca3a1a3
mhostetter <matthostetter@gmail.com>
2022-12-16 15:30:07 -0500

    Clean up conway poly script

--- scripts/create_conway_polys_database.py
+++ scripts/create_conway_polys_database.py
@@ -1,42 +1,74 @@
 """
-Script to create a database of Conway polynomials using Frank Luebeck's compilation of polynomials.
+A script to create a database of Conway polynomials using Frank Luebeck's compilation of polynomials.
 """
+from __future__ import annotations
+
 import os
 import sqlite3
 from pathlib import Path
 
 import requests
 
-POLY_TEXT_FILE_URL = "http://www.math.rwth-aachen.de/~Frank.Luebeck/data/ConwayPol/CPimport.txt"
-DATABASE_FILE = Path(__file__).parent.parent / "src" / "galois" / "_databases" / "conway_polys.db"
 
-if DATABASE_FILE.exists():
-    os.remove(DATABASE_FILE)
-
-conn = sqlite3.connect(DATABASE_FILE)
-cursor = conn.cursor()
-
-cursor.execute(
+def main():
     """
-    CREATE TABLE polys (
-        characteristic INTEGER NOT NULL,
-        degree INTEGER NOT NULL,
-        coefficients TEXT NOT NULL,
-        PRIMARY KEY (characteristic, degree)
+    The main routine to create a database of Conway polynomials.
+    """
+    text = requests.get("http://www.math.rwth-aachen.de/~Frank.Luebeck/data/ConwayPol/CPimport.txt").text
+
+    database_file = Path(__file__).parent.parent / "src" / "galois" / "_databases" / "conway_polys.db"
+    conn, cursor = create_database(database_file)
+
+    for line in text.splitlines():
+        # Skip first and last lines
+        if line in ["allConwayPolynomials := [", "0];"]:
+            continue
+
+        line = line[1:-2]  # Trim leading and trailing brackets
+        characteristic, degree, coefficients = line.split(",", maxsplit=2)
+        print(f"Conway polynomial for GF({characteristic}^{degree})")
+
+        add_to_database(cursor, characteristic, degree, coefficients)
+
+    conn.commit()
+    conn.close()
+
+
+def create_database(file: Path) -> tuple[sqlite3.Connection, sqlite3.Cursor]:
+    """
+    Deletes the old database, makes a new one, and returns the database connection.
+    """
+    if file.exists():
+        os.remove(file)
+
+    conn = sqlite3.connect(file)
+    cursor = conn.cursor()
+    create_table(conn, cursor)
+
+    return conn, cursor
+
+
+def create_table(conn: sqlite3.Connection, cursor: sqlite3.Cursor):
+    """
+    Creates an empty 'polys' table.
+    """
+    cursor.execute(
+        """
+        CREATE TABLE polys (
+            characteristic INTEGER NOT NULL,
+            degree INTEGER NOT NULL,
+            coefficients TEXT NOT NULL,
+            PRIMARY KEY (characteristic, degree)
+        )
+        """
     )
+    conn.commit()
+
+
+def add_to_database(cursor: sqlite3.Cursor, characteristic: str, degree: str, coefficients: str):
     """
-)
-
-text = requests.get(POLY_TEXT_FILE_URL).text
-for line in text.splitlines():
-    # Skip first and last lines
-    if line in ["allConwayPolynomials := [", "0];"]:
-        continue
-
-    line = line[1:-2]  # Trim leading and trailing brackets
-    characteristic, degree, coefficients = line.split(",", maxsplit=2)
-    print(f"Conway polynomial for GF({characteristic}^{degree})")
-
+    Adds the given Conway polynomial to the database.
+    """
     cursor.execute(
         """
         INSERT INTO polys (characteristic, degree, coefficients)
@@ -45,5 +77,6 @@ for line in text.splitlines():
         (int(characteristic), int(degree), coefficients),
     )
 
-conn.commit()
-conn.close()
+
+if __name__ == "__main__":
+    main()
