b3c4b5bc35d7d51434d56ee4d6e7ffd8d50e6697
mhostetter <matthostetter@gmail.com>
2023-01-24 17:00:33 -0500

    Memoize `Poly.is_primitive()`

--- src/galois/_polys/_primitive.py
+++ src/galois/_polys/_primitive.py
@@ -3,6 +3,7 @@ A module containing functions to generate and test primitive polynomials.
 """
 from __future__ import annotations
 
+import functools
 from typing import Iterator
 
 from typing_extensions import Literal
@@ -22,6 +23,7 @@ from ._search import (
 
 
 # NOTE: This is a method of the Poly class. It will be added to the Poly class in `polys/__init__.py`.
+@functools.lru_cache(maxsize=8192)
 def is_primitive(self) -> bool:
     r"""
     Determines whether the polynomial :math:`f(x)` over :math:`\mathrm{GF}(q)` is primitive.
