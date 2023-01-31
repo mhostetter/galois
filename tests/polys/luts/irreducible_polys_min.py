1f81b33d4be406311c709fe96cafad401121acd9
mhostetter <matthostetter@gmail.com>
2023-01-31 15:33:06 -0500

    Clean up polynomial LUTs

---
+++ tests/polys/luts/irreducible_polys_min.py
@@ -0,0 +1,19 @@
+"""
+A module containing LUTs for lexicographically-first irreducible polynomials with minimal terms.
+
+LUT items obtained by randomly picking degrees and checking the PDF,
+
+References:
+    - Gadiel Seroussi's table (1998). https://www.hpl.hp.com/techreports/98/HPL-98-135.pdf.
+"""
+
+# LUT items are (order, degree, nonzero_degrees, nonzero_coeffs) in degree-descending order.
+# sorted(numpy.random.default_rng(1337).integers(size=5, low=500, high=10_000, endpoint=True))
+
+IRREDUCIBLE_POLYS_MIN = [
+    (2, 2262, [[2262, 57, 0], [1, 1, 1]]),
+    (2, 5632, [[5632, 17, 15, 5, 0], [1, 1, 1, 1, 1]]),
+    (2, 5690, [[5690, 1623, 0], [1, 1, 1]]),
+    (2, 7407, [[7407, 27, 21, 17, 0], [1, 1, 1, 1, 1]]),
+    (2, 8842, [[8842, 4143, 0], [1, 1, 1]]),
+]
--- tests/polys/test_primitive_polys.py
+++ tests/polys/test_primitive_polys.py
@@ -10,70 +10,7 @@ import pytest
 
 import galois
 
-from .luts.primitive_polys_2 import (
-    PRIMITIVE_POLYS_2_1,
-    PRIMITIVE_POLYS_2_2,
-    PRIMITIVE_POLYS_2_3,
-    PRIMITIVE_POLYS_2_4,
-    PRIMITIVE_POLYS_2_5,
-    PRIMITIVE_POLYS_2_6,
-    PRIMITIVE_POLYS_2_7,
-    PRIMITIVE_POLYS_2_8,
-)
-from .luts.primitive_polys_3 import (
-    PRIMITIVE_POLYS_3_1,
-    PRIMITIVE_POLYS_3_2,
-    PRIMITIVE_POLYS_3_3,
-    PRIMITIVE_POLYS_3_4,
-    PRIMITIVE_POLYS_3_5,
-    PRIMITIVE_POLYS_3_6,
-)
-from .luts.primitive_polys_4 import (
-    PRIMITIVE_POLYS_4_1,
-    PRIMITIVE_POLYS_4_2,
-    PRIMITIVE_POLYS_4_3,
-)
-from .luts.primitive_polys_5 import (
-    PRIMITIVE_POLYS_5_1,
-    PRIMITIVE_POLYS_5_2,
-    PRIMITIVE_POLYS_5_3,
-    PRIMITIVE_POLYS_5_4,
-)
-from .luts.primitive_polys_9 import (
-    PRIMITIVE_POLYS_9_1,
-    PRIMITIVE_POLYS_9_2,
-    PRIMITIVE_POLYS_9_3,
-)
-from .luts.primitive_polys_25 import PRIMITIVE_POLYS_25_1, PRIMITIVE_POLYS_25_2
-
-PARAMS = [
-    (2, 1, PRIMITIVE_POLYS_2_1),
-    (2, 2, PRIMITIVE_POLYS_2_2),
-    (2, 3, PRIMITIVE_POLYS_2_3),
-    (2, 4, PRIMITIVE_POLYS_2_4),
-    (2, 5, PRIMITIVE_POLYS_2_5),
-    (2, 6, PRIMITIVE_POLYS_2_6),
-    (2, 7, PRIMITIVE_POLYS_2_7),
-    (2, 8, PRIMITIVE_POLYS_2_8),
-    (2**2, 1, PRIMITIVE_POLYS_4_1),
-    (2**2, 2, PRIMITIVE_POLYS_4_2),
-    (2**2, 3, PRIMITIVE_POLYS_4_3),
-    (3, 1, PRIMITIVE_POLYS_3_1),
-    (3, 2, PRIMITIVE_POLYS_3_2),
-    (3, 3, PRIMITIVE_POLYS_3_3),
-    (3, 4, PRIMITIVE_POLYS_3_4),
-    (3, 5, PRIMITIVE_POLYS_3_5),
-    (3, 6, PRIMITIVE_POLYS_3_6),
-    (3**2, 1, PRIMITIVE_POLYS_9_1),
-    (3**2, 2, PRIMITIVE_POLYS_9_2),
-    (3**2, 3, PRIMITIVE_POLYS_9_3),
-    (5, 1, PRIMITIVE_POLYS_5_1),
-    (5, 2, PRIMITIVE_POLYS_5_2),
-    (5, 3, PRIMITIVE_POLYS_5_3),
-    (5, 4, PRIMITIVE_POLYS_5_4),
-    (5**2, 1, PRIMITIVE_POLYS_25_1),
-    (5**2, 2, PRIMITIVE_POLYS_25_2),
-]
+from .luts.primitive_polys import PRIMITIVE_POLYS
 
 
 def test_primitive_poly_exceptions():
@@ -96,17 +33,17 @@ def test_primitive_poly_exceptions():
         galois.primitive_poly(2, 3, method="invalid-argument")
 
 
-@pytest.mark.parametrize("order,degree,polys", PARAMS)
+@pytest.mark.parametrize("order,degree,polys", PRIMITIVE_POLYS)
 def test_primitive_poly_min(order, degree, polys):
     assert galois.primitive_poly(order, degree).coeffs.tolist() == polys[0]
 
 
-@pytest.mark.parametrize("order,degree,polys", PARAMS)
+@pytest.mark.parametrize("order,degree,polys", PRIMITIVE_POLYS)
 def test_primitive_poly_max(order, degree, polys):
     assert galois.primitive_poly(order, degree, method="max").coeffs.tolist() == polys[-1]
 
 
-@pytest.mark.parametrize("order,degree,polys", PARAMS)
+@pytest.mark.parametrize("order,degree,polys", PRIMITIVE_POLYS)
 def test_primitive_poly_random(order, degree, polys):
     assert galois.primitive_poly(order, degree, method="random").coeffs.tolist() in polys
 
@@ -131,12 +68,12 @@ def test_primitive_polys_exceptions():
         next(galois.primitive_polys(2, 3, terms="invalid-argument"))
 
 
-@pytest.mark.parametrize("order,degree,polys", PARAMS)
+@pytest.mark.parametrize("order,degree,polys", PRIMITIVE_POLYS)
 def test_primitive_polys(order, degree, polys):
     assert [f.coeffs.tolist() for f in galois.primitive_polys(order, degree)] == polys
 
 
-@pytest.mark.parametrize("order,degree,polys", PARAMS)
+@pytest.mark.parametrize("order,degree,polys", PRIMITIVE_POLYS)
 def test_specific_terms(order, degree, polys):
     all_polys = []
     for terms in range(1, degree + 2):
@@ -154,7 +91,7 @@ def test_specific_terms_none_found():
     assert not list(galois.primitive_polys(2, 3, terms=2))
 
 
-@pytest.mark.parametrize("order,degree,polys", PARAMS)
+@pytest.mark.parametrize("order,degree,polys", PRIMITIVE_POLYS)
 def test_minimum_terms(order, degree, polys):
     min_terms = min(np.count_nonzero(f) for f in polys)
     min_term_polys = [f for f in polys if np.count_nonzero(f) == min_terms]
